4a9d00f929740614e5324fcd0900d6c09af1ce36
jacklendel <jackson.lendel@gmail.com>
2021-03-24 20:24:54 -0300

    cria pata dos projetos

---
+++ Projetos Frot End/index.php
@@ -0,0 +1,20 @@
+<html>
+
+	<head>
+		<title>Meu Primeirosite de PHP!</title>
+	</head>
+	
+	<body>
+	
+		<?php
+			for ( $i = 0 ; $i <10 ; $i++ )
+			{
+				print ("Linha número " . $i . "<br/>");
+			}
+		?>
+		
+	</body>
+
+</html>
+
+	
